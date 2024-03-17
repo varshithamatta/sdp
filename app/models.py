69757e7899fae05f74b9f8cc2630c106f9ac7b5a
varshithamatta <163740549+varshithamatta@users.noreply.github.com>
2024-03-17 19:04:16 +0530

    Delete app directory

--- app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class student(models.Model):
-    firstName = models.CharField(max_length=20)
-    lastName = models.CharField(max_length=20)
-    email = models.EmailField(max_length=100)
-    dob = models.DateField(max_length=20)
-    password = models.CharField(max_length=20)
